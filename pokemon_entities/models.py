c65da18c3e6ee8a256fbf060813087bf5719b8e6
axmetES <fraktsia@gmail.com>
2020-04-07 15:10:29 +0600

    -Changed image field in Pokemon model from file to url context. -Modefied show_all_pokemons func     pokemons_on_page = [] taked data from DB.

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Pokemon(models.Model):
     id = models.AutoField(auto_created=True, primary_key=True)
     title = models.CharField(max_length=200)
-    image = models.ImageField(upload_to='pokemons', null=True)
+    image = models.URLField()
     appeared_at = models.DateTimeField(null=True)
     disappeared_at = models.DateTimeField(null=True)
     level = models.IntegerField(default=True)
@@ -12,6 +12,7 @@ class Pokemon(models.Model):
     attack = models.IntegerField(default=True)
     defence = models.IntegerField(default=True)
     stamina = models.IntegerField(default=True)
+
     # entity = models.ForeignKey(on_delete=)
 
     def __str__(self):
@@ -22,4 +23,3 @@ class PokemonEntity(models.Model):
     lat = models.FloatField(null=True)
     lon = models.FloatField(null=True)
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
-
--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -24,9 +24,15 @@ def add_pokemon(folium_map, lat, lon, name, image_url=DEFAULT_IMAGE_URL):
 def show_all_pokemons(request):
     with open("pokemon_entities/pokemons.json", encoding="utf-8") as database:
         pokemons = json.load(database)['pokemons']
-    pokemons = Pokemon.objects.all()
 
-    pokemon_entities = PokemonEntity.objects.all()
+
+    pokemons_obj = Pokemon.objects.all()
+    # for pokemon in pokemons_obj:
+    #     title = pokemon.title
+    #     pokemon_entity = PokemonEntity.objects.filter(pokemon__title__contains=title)
+    #     for entity in pokemon_entity:
+    #         add_pokemon(folium_map, entity.lat, entity.lon, pokemon.title, pokemon.image)
+
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
     for pokemon in pokemons:
         for pokemon_entity in pokemon['entities']:
@@ -35,11 +41,11 @@ def show_all_pokemons(request):
                 pokemon['title_ru'], pokemon['img_url'])
 
     pokemons_on_page = []
-    for pokemon in pokemons:
+    for pokemon in pokemons_obj:
         pokemons_on_page.append({
-            'pokemon_id': pokemon['pokemon_id'],
-            'img_url': pokemon['img_url'],
-            'title_ru': pokemon['title_ru'],
+            'pokemon_id': pokemon.id,
+            'img_url': pokemon.image,
+            'title_ru': pokemon.title,
         })
 
     return render(request, "mainpage.html", context={
